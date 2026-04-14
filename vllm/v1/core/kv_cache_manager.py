8492da3e31b3a31954577a0a0a34799464175236
GDzhu01 <809721801@qq.com>
2026-04-14 20:22:11 +0800

    delete print
    Signed-off-by: GDzhu01 <809721801@qq.com>

--- vllm/v1/core/kv_cache_manager.py
+++ vllm/v1/core/kv_cache_manager.py
@@ -344,10 +344,7 @@ class KVCacheManager:
             + num_external_computed_tokens,
             num_tokens_main_model=num_tokens_main_model,
         )
-
-        print(60*"=")
-        print(f"free blocks: {self.block_pool.get_num_free_blocks()}")
-        print(f"{num_blocks_to_allocate=}")
+        
         if num_blocks_to_allocate > self.block_pool.get_num_free_blocks():
             print(f"Cannot allocate new blocks")
             # Cannot allocate new blocks
